d707899375988e2d787783609de500b19057f44f
Chen Charles <lchenbg@ust.hk>
2017-11-20 19:17:44 +0800

    d

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/CouponWarehouse.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/CouponWarehouse.java
@@ -283,18 +283,18 @@ public class CouponWarehouse{
   * @return whether a user is qualified to get coupon from entering code
   */
   public boolean canGetCouponFromCode(Users user){
-    /*String a = "";
+    String a = "";
     for(String nu:newUids) a += "@@"+ nu;
     String b = "";
     for(String nu:existingUids) b += "@@"+ nu;
     log.info("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
     log.info(a);
     log.info("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-    log.info(b);*/
+    log.info(b);
     if (isNewUser(user)){
-      /*log.info("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+      log.info("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
       log.info("User is not old");
-      log.info("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");*/
+      log.info("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
       return (!gotCouponNewUsers.contains(user.getID()));
     }
     else return false;
